c1e40045d16c9c0728cb8dac677c1db702906dae
slicingmelon <petre@tutanota.com>
2024-12-02 03:57:03 +0200

    more helpers and stuff

--- helpers.go
+++ helpers.go
@@ -41,12 +41,95 @@ func (u *RawURL) FullString() string {
 	return buf.String()
 }
 
+// GetRawScheme reconstructs the scheme from its components
+func GetRawSche(u *RawURL) string {
+	// xx
+	//return buf.string()
+}
+
+// GetRawUserInfo reconstructs the userinfo from its components
+func GetRawUserInfo(u *RawURL) string {
+	var buf strings.Builder
+	// xx
+	//return buf.string()
+}
+
+// GetRawAuthority reconstructs the authority from its components
+func GetRawAuthority(u *RawURL) string {
+	return u.Host
+}
+
+// GetRawHostname reconstructs the hostname of the URL (without port)
+func GetRawHostname(u *RawURL) string {
+	if i := strings.LastIndex(u.Host, ":"); i != -1 {
+		return u.Host[:i]
+	}
+	return u.Host
+}
+
+// GetRawPort reconstructs the port of the URL
+func GetRawPort(u *RawURL) string {
+	if i := strings.LastIndex(u.Host, ":"); i != -1 {
+		return u.Host[i+1:]
+	}
+	return ""
+}
+
+// GetRawPath reconstructs the path from its components
+func GetRawPath(u *RawURL) string {
+	// xx
+}
+
+// GetRawPath reconstructs the path from its components
+// Similar to GetRawPath but can omit first / in path
+func GetRawPathUnsafe(u *RawURL) string {
+	// xx
+}
+func GetRawQuery(u *RawURL) string {
+	// xx
+}
+
+// GetRawFragment reconstructs the fragment from its components
+func GetRawFragment(u *RawURL) string {
+	// xx
+}
+
+// QueryValues returns a map of query parameters
+func (u *RawURL) GetRawQueryValues() map[string][]string {
+	values := make(map[string][]string)
+	for _, pair := range strings.Split(u.Query, "&") {
+		if pair == "" {
+			continue
+		}
+		kv := strings.SplitN(pair, "=", 2)
+		key := kv[0]
+		value := ""
+		if len(kv) == 2 {
+			value = kv[1]
+		}
+		values[key] = append(values[key], value)
+	}
+	return values
+}
+
 // ...
-// https://media.beehiiv.com/cdn-cgi/image/fit=scale-down,format=auto,onerror=redirect,quality=80/uploads/asset/file/fac6de4c-8a4f-4688-aa43-0fbcf784426e/url-structure-and-scheme-2022.png
+// scheme://host/path
 func (u *RawURL) GetFullRawURI() string {
 	//..
 }
 
+/*
+--->  scheme://host/path?query#fragment
+
+	             userinfo      host       port		path		       query		     fragment
+	            |------ ||--------------||---||--------------||-------------------------||---|
+		https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top
+		|----|  |---------------------------|
+		scheme         authority
+*/
+func (u *RawURL) GetFullRawURL() string {
+	//..
+}
 func (u *RawURL) GetRawRelativeURI() string {
 	//..
 }
@@ -71,38 +154,4 @@ func lastIndexRune(s string, r rune) int {
 		i -= size
 	}
 	return -1
-}
-
-// Port returns the port of the URL
-func (u *RawURL) Port() string {
-	if i := strings.LastIndex(u.Host, ":"); i != -1 {
-		return u.Host[i+1:]
-	}
-	return ""
-}
-
-// Hostname returns the hostname of the URL (without port)
-func (u *RawURL) Hostname() string {
-	if i := strings.LastIndex(u.Host, ":"); i != -1 {
-		return u.Host[:i]
-	}
-	return u.Host
-}
-
-// QueryValues returns a map of query parameters
-func (u *RawURL) QueryValues() map[string][]string {
-	values := make(map[string][]string)
-	for _, pair := range strings.Split(u.Query, "&") {
-		if pair == "" {
-			continue
-		}
-		kv := strings.SplitN(pair, "=", 2)
-		key := kv[0]
-		value := ""
-		if len(kv) == 2 {
-			value = kv[1]
-		}
-		values[key] = append(values[key], value)
-	}
-	return values
 }
